3649bc01832c22eebf3c3c4e04e4b597338742d4
David Capela <davidcapela@MacBook-Pro-de-David-1517.local>
2019-12-11 23:08:26 +0000

    Adicionado explicador controlador e servico

---
+++ projeto/src/main/java/esof/projeto/services/ExplicadorService.java
@@ -0,0 +1,14 @@
+package esof.projeto.services;
+
+
+import esof.projeto.repositories.ExplicadorRepo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExplicadorService {
+
+
+    private ExplicadorRepo explicadorRepo;
+
+
+}
